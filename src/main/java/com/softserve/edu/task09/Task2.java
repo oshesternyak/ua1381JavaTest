d0ae61f1c594018a8ae91f90babeb0ca72753599
OlhaS <oshesternyak@gmail.com>
2025-06-25 21:04:02 -0400

    implement task2

---
+++ src/main/java/com/softserve/edu/task09/Task2.java
@@ -0,0 +1,72 @@
+package com.softserve.edu.task09;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Task2 {
+    public List<Integer> generateList(int size) {
+        Random random = new Random();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randomInt = random.nextInt(100);
+            list.add(randomInt);
+        }
+        return list;
+    }
+
+    public void printAll(List<Integer> list) {
+        for (int n : list) {
+            System.out.println(n);
+        }
+    }
+
+    public int findMin(List<Integer> list) {
+        int min = Integer.MAX_VALUE;
+        for (int num : list) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    public int findMax(List<Integer> list) {
+        int max = Integer.MIN_VALUE;
+        for (int num : list) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public double calculateAverage(List<Integer> list) {
+        int sum = 0;
+        for (int num : list) {
+            sum = sum + num;
+        }
+        return sum/ list.size();
+    }
+
+    public List<Integer> removeAllEven(List<Integer> list) {
+
+        return new ArrayList<>();
+    }
+
+    public boolean containsNum(List<Integer> list, int num) {
+        for (int i = 0; i <list.size(); i++) {
+            int n = list.get(i);
+            if (n == num){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public void sort(List<Integer> list) {
+
+    }
+}
---
+++ src/test/java/com/softserve/edu/task09/Task2Test.java
@@ -0,0 +1,79 @@
+package com.softserve.edu.task09;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2Test {
+    private Task2 task2 = new Task2();
+
+    @BeforeEach
+    public void before_each() {
+        task2 = new Task2();
+    }
+
+    @Test
+    void generateList() {
+        List<Integer> list = task2.generateList(10);
+        Assertions.assertEquals(10, list.size());
+    }
+
+    @Test
+    void findMin() {
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(29);
+       int actual= task2.findMin(list);
+       Assertions.assertEquals(5, actual);
+    }
+
+    @Test
+    void findMax() {
+        List<Integer> list = new ArrayList<>();
+        list.add(8);
+        list.add(18);
+        list.add(81);
+        int actual = task2.findMax(list);
+        Assertions.assertEquals(81, actual);
+    }
+
+    @Test
+    void calculateAverage() {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(16);
+        list.add(1);
+        list.add(30);
+        list.add(3);
+        double expected = 12.0;
+        double actual = task2.calculateAverage(list);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+//    @Test
+//    void removeAllEven() {
+//    }
+
+    @Test
+    void containsNum() {
+        List<Integer> list = new ArrayList<>();
+        list.add(79);
+        list.add(8);
+        boolean actual = task2.containsNum(list, 8);
+        Assertions.assertEquals(true, actual);
+
+    }
+
+    @Test
+    void sort() {
+    }
+}
